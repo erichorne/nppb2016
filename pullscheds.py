f5ab3fb29cf72bc7366a8dc8bce1e597e85f6d3b
Ubuntu <ubuntu@box11.org>
2016-03-26 14:49:17 +0000

    March 26 update

---
+++ pullscheds.py
@@ -0,0 +1,5 @@
+python ./sched.py pinto-american | sort -u > pinto-american.csv
+python ./sched.py mustang-american | sort -u > mustang-american.csv
+python ./sched.py bronco-american | sort -u > bronco-american.csv
+python ./sched.py pony-american | sort -u > pony-american.csv
+python ./sched.py pony-national | sort -u > pony-national.csv
